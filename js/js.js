1e97aeba6e2265644b128a01c62edf20e3dfdfb8
andres.fandino <andresf084@gmail.com>
2021-10-08 19:27:56 -0500

    Update json

---
+++ js/js.js
@@ -0,0 +1,5 @@
+function multiplicacion(multiplicando1, multiplicando2) {
+    return multiplicando1 * multiplicando2;
+}
+var resultadoMultiplicacion = multiplicacion(8, 3);
+console.log(resultadoMultiplicacion);
